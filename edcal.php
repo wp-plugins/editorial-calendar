e029c5e9562e8c5c758f54c0eef76b213c192dd0
zgrossbart <zgrossbart@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-18 13:42:20 +0000

    Added the hover class for loading posts.
    git-svn-id: https://plugins.svn.wordpress.org/editorial-calendar/trunk@174938 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edcal.php
+++ edcal.php
@@ -93,6 +93,10 @@ function edcal_list_admin() {
             background-image: url('<?php echo(path_join(WP_PLUGIN_URL, basename( dirname( __FILE__ ) )."/images/loading_post.gif")); ?>');
         }
 
+        .loadingclass:hover {
+            background-image: url('<?php echo(path_join(WP_PLUGIN_URL, basename( dirname( __FILE__ ) )."/images/loading_post_hover.gif")); ?>');
+        }
+
         #loading {
             background-image: url('<?php echo(path_join(WP_PLUGIN_URL, basename( dirname( __FILE__ ) )."/images/loading.gif")); ?>');
         }
