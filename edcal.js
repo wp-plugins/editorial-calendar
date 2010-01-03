7fe928b6fcc45a5a5f8b637c85a4c36e56b3e98c
MaryVogt <MaryVogt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-03 13:19:25 +0000

    We are now using the built in encodeURIComponent function instead of the JQuery function since the JQuery one was not handling wide characters properly.
    git-svn-id: https://plugins.svn.wordpress.org/editorial-calendar/trunk@189221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edcal.js
+++ edcal.js
@@ -386,7 +386,7 @@ var edcal = {
     saveTitle: function(/*string*/ postId) {
          edcal.output("Saving the new title " + jQuery("#edcal-title-edit-field").val() + " for post " + postId);
          var url = edcal.ajax_url + "&action=edcal_changetitle&postid=" + postId + 
-             "&title=" + jQuery.URLEncode(jQuery("#edcal-title-edit-field").val());
+             "&title=" + encodeURIComponent(jQuery("#edcal-title-edit-field").val());
 
          jQuery("#post-" + postId).addClass("loadingclass");
          
@@ -1077,10 +1077,10 @@ var edcal = {
           * We don't really let them set a time in the calendar, so we 
           * put a default post time of 10:00 AM.
           */
-         var formattedDate = jQuery.URLEncode(edcal.getDayFromDayId(date).toString("yyyy-dd-MM") + " 10:00:00");
+         var formattedDate = encodeURIComponent(edcal.getDayFromDayId(date).toString("yyyy-dd-MM") + " 10:00:00");
          edcal.output("Creating post on: " + edcal.getDayFromDayId(date).toString("yyyy-dd-MM"));
          var url = edcal.ajax_url + "&action=edcal_newdraft&date=" + formattedDate + "&title=" + 
-             jQuery.URLEncode(title);
+             encodeURIComponent(title);
 
          jQuery.ajax( { 
             url: url,
@@ -1339,17 +1339,3 @@ var edcal = {
 jQuery(document).ready(function(){
     edcal.init();
 });
-
-/*
- * This code is from the JQuery URL Encode plugin (http://plugins.jquery.com/project/URLEncode);
- */
-jQuery.extend({URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a-zA-Z0-9_.]*)/;
-  while(x<c.length){var m=r.exec(c.substr(x));
-    if(m!==null && m.length>1 && m[1]!=''){o+=m[1];x+=m[1].length;
-    }else{if(c[x]===' '){o+='+';}else{var d=c.charCodeAt(x);var h=d.toString(16);
-    o+='%'+(h.length<2?'0':'')+h.toUpperCase();}x++;}}return o;},
-URLDecode:function(s){var o=s;var binVal,t;var r=/(%[^%]{2})/;
-  while((m=r.exec(o))!==null && m.length>1 && m[1]!==''){b=parseInt(m[1].substr(1),16);
-  t=String.fromCharCode(b);o=o.replace(m[1],t);}return o;}
-});
-
