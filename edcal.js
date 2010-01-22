6c6fb27a28bb90bde672b777878616a92e495b39
MaryVogt <MaryVogt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-22 12:47:47 +0000

    Cleaned up the height code.
    git-svn-id: https://plugins.svn.wordpress.org/editorial-calendar/trunk@196620 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edcal.js
+++ edcal.js
@@ -827,19 +827,7 @@ var edcal = {
        calendar based on the window height.
      */
     getCalHeight: function() {
-        var myHeight = 0;
-        if ( typeof( window.innerWidth ) == 'number' ) {
-            //Non-IE
-            myHeight = window.innerHeight;
-        } else if ( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-            //IE 6+ in 'standards compliant mode'
-            myHeight = document.documentElement.clientHeight;
-        } else if ( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-            //IE 4 compatible
-            myHeight = document.body.clientHeight;
-        }
-        
-        myHeight = myHeight - 225
+        var myHeight = jQuery(window).height() - 225;
         
         /*
            We don't want to make the calendar too short even if the
