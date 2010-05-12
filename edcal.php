7ce36a74420591687565845b6835a80650b03cf1
zgrossbart <zgrossbart@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-12 17:57:18 +0000

    Updated the version to 0.8.
    git-svn-id: https://plugins.svn.wordpress.org/editorial-calendar/trunk@240274 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edcal.php
+++ edcal.php
@@ -18,7 +18,7 @@
 /*
 Plugin Name: WordPress Editorial Calendar
 Description: An editorial calendar for managing the dates of your WordPress posts
-Version: 0.7
+Version: 0.8
 Author: Mary Vogt and Zack Grossbart
 Author URI: http://www.zackgrossbart.com
 */
