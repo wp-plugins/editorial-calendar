5c9b944bc49f0bdb7fd034005c0295a2138ac6fb
zgrossbart <zgrossbart@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-07 00:35:12 +0000

    Fixed a string escape issue in the feedback string.
    git-svn-id: https://plugins.svn.wordpress.org/editorial-calendar/trunk@260940 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- edcal.php
+++ edcal.php
@@ -248,7 +248,7 @@ function edcal_list_admin() {
 
             edcal.str_feedbackmsg = <?php echo(edcal_json_encode(__('<div id="feedbacksection">' . 
              '<h2>Let us collect a little data</h2><br />' .
-             'We are always trying to improve the Editorial Calendar and you can help. May we collect some anonymous data about your blog and browser settings to help us improve this plugin?  We'll only do it once.<br /><br />' . 
+             'We are always trying to improve the Editorial Calendar and you can help. May we collect some anonymous data about your blog and browser settings to help us improve this plugin?  We\'ll only do it once.<br /><br />' . 
              '<button class="button-secondary" onclick="edcal.doFeedback();">Collect Anonymous Data</button> ' . 
              '<a href="#" onclick="edcal.noFeedback(); return false;">No thank you</a></div>', 'editorial-calendar'))) ?>;
 
